99072f29389c373828c62e6b68c1ce1014066bf7
Stefan <Stefan@DESKTOP-RD5IAF5>
2020-03-08 20:53:53 +0100

    cos zrobilem

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+            Console.WriteLine(response);
+        }
+    }
+}
